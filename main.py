94879a07f9259bb75c3d46d5ccd2f67422d0fdfc
rkrukshan <rukshan1122@gmail.com>
2026-08-09 15:53:47 +0530

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from dotenv import load_dotenv  
-from pydantic import baseModel
+from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 load_dotenv()
+
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used:list[str]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+parser = PydanticOutput(pydantic_object=ResearchResponse)
+
+# response = llm.invoke("what is the meaning of life?")
+# print(response.content)
+
+
